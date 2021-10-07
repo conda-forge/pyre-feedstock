2f7817d688ada81a7cca07f6937d4b3bd3bcb8bd
Ryan Burns <rtburns@jpl.nasa.gov>
2021-10-07 11:10:24 -0700

    Use absolute python install path

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,18 +11,13 @@ fi
 
 mkdir build && cd build
 
-# Strip install prefix (and '/') to compute relative $SP_DIR
-# Remove after backporting https://github.com/pyre/pyre/pull/60
-relpath(){ $PYTHON -c "print('$1'[len('$2')+1:])"; }
-pypkgrel=$(relpath $SP_DIR $PREFIX)
-
 # Override LIBDIR from GNUInstallDirs - would be lib64 on centos
 # Specify $PYTHON to avoid using system python
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_LIBDIR=lib \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DPYRE_DEST_PACKAGES=$pypkgrel \
+    -DPYRE_DEST_PACKAGES=$SP_DIR \
     -DPython_EXECUTABLE=$PYTHON \
     -DPYRE_VERSION=$PKG_VERSION \
     $SRC_DIR
