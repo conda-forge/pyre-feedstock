43f03810c05d29542bb21df0c765229b8d7f3713
Ryan Burns <rtburns@jpl.nasa.gov>
2021-11-09 15:36:03 -0800

    Move ctest command to separate test phase

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,9 +23,3 @@ cmake \
     $SRC_DIR
 
 cmake --build . --target install -j${CPU_COUNT}
-
-# Disable TCP test on Docker
-# Reenable locale_codec after resolving https://github.com/pyre/pyre/issues/65
-ctest \
-    -E '(pyre.pkg.ipc.tcp.py|python.locale_codec.py|pyre.pkg.codecs.yaml_|pyre.lib.timers.process_timer|pyre.pkg.timers)' \
-    --output-on-failure
